01239032a70426fdb3d8fcde3182cfcfee5a5abc
Israel Jorge <98141391+IsraelJorge@users.noreply.github.com>
2022-03-13 13:29:39 -0300

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const listaSelacaoPokemon = document.querySelectorAll('.pokemon')
+const pokemonsCard = document.querySelectorAll('.cartao-pokemon')
+
+listaSelacaoPokemon.forEach(pokemon => {
+    
+    pokemon.addEventListener('click', () => {
+        const cartaoPokemonAberto = document.querySelector('.aberto')
+        cartaoPokemonAberto.classList.remove('aberto')
+        
+        const idPokemonSelecionado = pokemon.attributes.id.value
+        
+        const idDoCartaoParaAbrir = 'cartao-' + idPokemonSelecionado
+        const cartaoPokemonParaAbrir = document.getElementById(idDoCartaoParaAbrir)
+        cartaoPokemonParaAbrir.classList.add('aberto')
+
+        const pokemonAtivoNaListagem = document.querySelector('.ativo')
+        pokemonAtivoNaListagem.classList.remove('ativo')
+
+        const pokemonSelecionadoNaListagem = document.getElementById(idPokemonSelecionado)
+        pokemonSelecionadoNaListagem.classList.add('ativo')
+    })
+})
